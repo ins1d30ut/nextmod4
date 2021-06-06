54fdb537cc4c8ee82f8bafa04c7472cf3c0ca6d5
ins1d30ut <bmoses001@protonmail.com>
2021-06-06 10:53:52 -0500

    commit event item and index

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,11 +1,23 @@
 import EventItem from './event-item';
+// import classes from './event-list.module.css';
 
 function EventList(props) {
   const { items } = props;
 
-  return <ul>
-    {items.map(event => <li></li>)}
-  </ul>
+  return (
+    <ul>
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
+      ))}
+    </ul>
+  );
 }
 
 export default EventList;
